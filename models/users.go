7e4750eb94940498e1065747a66f1b3e811fe859
Nuno Honório <nh@kamae.pt>
2024-09-29 17:38:58 +0100

    users api and routes setup

--- models/users.go
+++ models/users.go
@@ -1,1 +1,18 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID           primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Phone        int64              `json:"phone,omitempty" bson:"phone,omitempty"`
+	Name         string             `json:"name,omitempty" bson:"name,omitempty"`
+	Password     string             `json:"password,omitempty" bson:"password,omitempty"`
+	Role         string             `json:"role,omitempty" bson:"role,omitempty"`
+	Locality     string             `json:"locality,omitempty" bson:"locality,omitempty"`
+	Profession   string             `json:"profission,omitempty" bson:"profission,omitempty"`
+	PricePerHour float64            `json:"price_hour,omitempty" bson:"price_hour,omitempty"`
+	Rating       float64            `json:"rating,omitempty" bson:"rating,omitempty"`
+	Fee          float64            `json:"fee,omitempty" bson:"fee,omitempty"`
+	JobsDone     int64              `json:"jobs_done,omitempty" bson:"jobs_done,omitempty"`
+	Block        bool               `json:"block,omitempty" bson:"block,omitempty"`
+}
--- routes/users.go
+++ routes/users.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"PSbackend/api"
+	"context"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func UserRoutes(ctx context.Context, client *mongo.Client, dbName, userCollection string, router *mux.Router) {
+	// Define route for creating a new user
+	router.HandleFunc("/api/users", func(w http.ResponseWriter, r *http.Request) {
+		api.CreateUser(ctx, client, dbName, userCollection, w, r)
+	}).Methods("POST")
+}
