80c83cdde73dee14711f5d2dedb7dc849e482ae2
Nuno Honório <nh@kamae.pt>
2024-09-29 18:36:00 +0100

    services api and routes setup and fetch all users

--- api/services.go
+++ api/services.go
@@ -1,1 +1,29 @@
 package api
+
+import (
+	"PSbackend/models"
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// CreateService handles POST requests to create a new service
+func CreateService(ctx context.Context, client *mongo.Client, dbName, serviceCollection string, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var service models.Services
+
+	json.NewDecoder(r.Body).Decode(&service)
+	service.ID = primitive.NewObjectID()
+
+	collection := client.Database(dbName).Collection(serviceCollection)
+
+	result, err := collection.InsertOne(ctx, service)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(result)
+}
--- models/services.go
+++ models/services.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Services struct {
+	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	EmployeeID string             `json:"employee_id,omitempty" bson:"employee_id,omitempty"`
+	EmployerID string             `json:"employer_id,omitempty" bson:"employer_id,omitempty"`
+	Value      float64            `json:"value,omitempty" bson:"value,omitempty"`
+	Status     string             `json:"status,omitempty" bson:"status,omitempty"`
+	Start      time.Time          `json:"start,omitempty" bson:"start,omitempty"`
+	End        time.Time          `json:"end,omitempty" bson:"end,omitempty"`
+}
