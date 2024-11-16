6c2ca185045595794e5ed04bcd4d7e3d292658e6
Nuno Honório <nh@kamae.pt>
2024-11-16 15:16:51 +0000

    value to Service

--- models/services.go
+++ models/services.go
@@ -11,6 +11,7 @@ type Services struct {
 	EmployeeID  string             `json:"employee_id,omitempty" bson:"employee_id,omitempty"`
 	ServiceType ServiceType        `json:"service_type,omitempty" bson:"service_type,omitempty"`
 	Description string             `json:"description,omitempty" bson:"description,omitempty"`
+	Value       float64            `json:"value,omitempty" bson:"value,omitempty"`
 	Appointment []Appointment      `json:"appointments,omitempty" bson:"appointments,omitempty"`
 }
 
@@ -23,7 +24,6 @@ type Appointment struct {
 	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	EmployeeID string             `json:"employee_id,omitempty" bson:"employee_id,omitempty"`
 	EmployerID string             `json:"employer_id,omitempty" bson:"employer_id,omitempty"`
-	Value      float64            `json:"value,omitempty" bson:"value,omitempty"`
 	Status     string             `json:"status,omitempty" bson:"status,omitempty"`
 	Start      time.Time          `json:"start,omitempty" bson:"start,omitempty"`
 	End        time.Time          `json:"end,omitempty" bson:"end,omitempty"`
