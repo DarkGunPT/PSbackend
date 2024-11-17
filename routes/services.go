3483ba3d764dd89ffea8fe9797c9cd20d484c857
Nuno Honório <nh@kamae.pt>
2024-11-17 15:36:32 +0000

    change to api/v1 endpoints

--- routes/services.go
+++ routes/services.go
@@ -11,87 +11,87 @@ import (
 
 func ServiceRoutes(client *mongo.Client, dbName, serviceCollection string, router *mux.Router) {
 	// Define route for creating a new service for Back Office
-	router.HandleFunc("/bo/services", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/services", func(w http.ResponseWriter, r *http.Request) {
 		api.CreateService(client, dbName, serviceCollection, w, r)
 	}).Methods("POST")
 
 	// Define route for creating a new service for Mobile App
-	router.HandleFunc("/mb/services", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/services", func(w http.ResponseWriter, r *http.Request) {
 		api.CreateService(client, dbName, serviceCollection, w, r)
 	}).Methods("POST")
 
 	// Define route for getting all services for Back Office
-	router.HandleFunc("/bo/services", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/services", func(w http.ResponseWriter, r *http.Request) {
 		api.GetServices(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 
 	// Define route for getting all services for Mobile App
-	router.HandleFunc("/mb/services", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/services", func(w http.ResponseWriter, r *http.Request) {
 		api.GetServices(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get a service by id for Back Office
-	router.HandleFunc("/bo/services/id", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/services/id", func(w http.ResponseWriter, r *http.Request) {
 		api.GetService(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get a service by id for Mobile App
-	router.HandleFunc("/mb/services/id", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/services/id", func(w http.ResponseWriter, r *http.Request) {
 		api.GetService(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 
 	// Define route for getting all filtered services by type for Back Office
-	router.HandleFunc("/bo/services/service-type", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/services/service-type", func(w http.ResponseWriter, r *http.Request) {
 		api.GetFilteredServiceType(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 
 	// Define route for getting all filtered services by type for Mobile App
-	router.HandleFunc("/mb/services/service-type", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/services/service-type", func(w http.ResponseWriter, r *http.Request) {
 		api.GetFilteredServiceType(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to update a service for Back Office
-	router.HandleFunc("/bo/services", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/services", func(w http.ResponseWriter, r *http.Request) {
 		api.UpdateService(client, dbName, serviceCollection, w, r)
 	}).Methods("PUT")
 
 	// Define route to update a service for Mobile App
-	router.HandleFunc("/mb/services", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/services", func(w http.ResponseWriter, r *http.Request) {
 		api.UpdateService(client, dbName, serviceCollection, w, r)
 	}).Methods("PUT")
 
 	// Define route for creating a new specific service type for Back Office
-	router.HandleFunc("/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
 		api.CreateServiceType(client, dbName, os.Getenv("SERVICE_TYPE_COLLECTION"), w, r)
 	}).Methods("POST")
 
 	// Define route for getting all services types for Back Office
-	router.HandleFunc("/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
 		api.GetServiceType(client, dbName, os.Getenv("SERVICE_TYPE_COLLECTION"), w, r)
 	}).Methods("GET")
 
 	// Define route for getting all services types for Mobile App
-	router.HandleFunc("/mb/service-type", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/service-type", func(w http.ResponseWriter, r *http.Request) {
 		api.GetServiceType(client, dbName, os.Getenv("SERVICE_TYPE_COLLECTION"), w, r)
 	}).Methods("GET")
 
 	// Define route to update a service type for Back Office
-	router.HandleFunc("/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
 		api.UpdateServiceType(client, dbName, os.Getenv("SERVICE_TYPE_COLLECTION"), w, r)
 	}).Methods("PUT")
 
 	// Define route to delete a service type by id for Back Office
-	router.HandleFunc("/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/service-type", func(w http.ResponseWriter, r *http.Request) {
 		api.DeleteServiceType(client, dbName, os.Getenv("SERVICE_TYPE_COLLECTION"), w, r)
 	}).Methods("DELETE")
 
 	// Define route to get services by technician for mobile
-	router.HandleFunc("/mb/services/technicians", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/services/technicians", func(w http.ResponseWriter, r *http.Request) {
 		api.GetServiceByTechnician(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get services by technician for Back Office
-	router.HandleFunc("/bo/services/technicians", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/services/technicians", func(w http.ResponseWriter, r *http.Request) {
 		api.GetServiceByTechnician(client, dbName, serviceCollection, w, r)
 	}).Methods("GET")
 }
--- routes/users.go
+++ routes/users.go
@@ -11,117 +11,117 @@ import (
 
 func UserRoutes(client *mongo.Client, dbName, userCollection string, router *mux.Router) {
 	// Define route to get users for mobile
-	router.HandleFunc("/mb/users", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users", func(w http.ResponseWriter, r *http.Request) {
 		api.GetUsers(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get users for backoffice
-	router.HandleFunc("/bo/users", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users", func(w http.ResponseWriter, r *http.Request) {
 		api.GetUsers(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get a user by phone  for mobile
-	router.HandleFunc("/mb/users/phone", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/phone", func(w http.ResponseWriter, r *http.Request) {
 		api.GetUser(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get a user by phone for backoffice
-	router.HandleFunc("/bo/users/phone", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/phone", func(w http.ResponseWriter, r *http.Request) {
 		api.GetUser(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to update a user for mobile
-	router.HandleFunc("/mb/users", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users", func(w http.ResponseWriter, r *http.Request) {
 		api.UpdateUser(client, dbName, userCollection, w, r)
 	}).Methods("PUT")
 
 	// Define route to update a user for backoffice
-	router.HandleFunc("/bo/users", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users", func(w http.ResponseWriter, r *http.Request) {
 		api.UpdateUser(client, dbName, userCollection, w, r)
 	}).Methods("PUT")
 
 	// Define route to delete a user by phone for mobile
-	router.HandleFunc("/mb/users", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users", func(w http.ResponseWriter, r *http.Request) {
 		api.DeleteUser(client, dbName, userCollection, w, r)
 	}).Methods("DELETE")
 
 	// Define route to delete a user by phone for backoffice
-	router.HandleFunc("/bo/users", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users", func(w http.ResponseWriter, r *http.Request) {
 		api.DeleteUser(client, dbName, userCollection, w, r)
 	}).Methods("DELETE")
 
 	// Define route for user login of mobile
-	router.HandleFunc("/mb/users/login", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/login", func(w http.ResponseWriter, r *http.Request) {
 		api.Login(client, dbName, userCollection, w, r)
 	}).Methods("POST")
 
 	// Define route for admin login of backoffice
-	router.HandleFunc("/bo/users/login", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/login", func(w http.ResponseWriter, r *http.Request) {
 		api.LoginAdmin(client, dbName, userCollection, w, r)
 	}).Methods("POST")
 
 	// Define route for creating a new role for backoffice
-	router.HandleFunc("/bo/users/role", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/role", func(w http.ResponseWriter, r *http.Request) {
 		api.CreateRole(client, dbName, os.Getenv("ROLES_COLLECTION"), w, r)
 	}).Methods("POST")
 
 	// Define route to get technicians for mobile
-	router.HandleFunc("/mb/users/technicians", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/technicians", func(w http.ResponseWriter, r *http.Request) {
 		api.GetTechnicians(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get technicians for backoffice
-	router.HandleFunc("/bo/users/technicians", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/technicians", func(w http.ResponseWriter, r *http.Request) {
 		api.GetTechnicians(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get clients for mobile
-	router.HandleFunc("/mb/users/clients", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/clients", func(w http.ResponseWriter, r *http.Request) {
 		api.GetClients(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to get clients for backoffice
-	router.HandleFunc("/bo/users/clients", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/clients", func(w http.ResponseWriter, r *http.Request) {
 		api.GetClients(client, dbName, userCollection, w, r)
 	}).Methods("GET")
 
 	// Define route to send email with a code to verify for mobile
-	router.HandleFunc("/mb/users/email", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/email", func(w http.ResponseWriter, r *http.Request) {
 		api.VerificateEmail(w, r, client, dbName, userCollection)
 	}).Methods("POST")
 
 	// Define route to send email with a code to verify for backoffice
-	router.HandleFunc("/bo/users/email", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/email", func(w http.ResponseWriter, r *http.Request) {
 		api.VerificateEmail(w, r, client, dbName, userCollection)
 	}).Methods("POST")
 
 	// Define route to confirm the code set by the user and define the new password for mobile
-	router.HandleFunc("/mb/users/email-confirmation", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/email-confirmation", func(w http.ResponseWriter, r *http.Request) {
 		api.ConfirmAuthCode(client, dbName, userCollection, w, r)
 	}).Methods("POST")
 
 	// Define route to confirm the code set by the user and define the new password for backoffice
-	router.HandleFunc("/bo/users/email-confirmation", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/email-confirmation", func(w http.ResponseWriter, r *http.Request) {
 		api.ConfirmAuthCode(client, dbName, userCollection, w, r)
 	}).Methods("POST")
 
 	// Define route to send recovery email with a code to verify for mobile
-	router.HandleFunc("/mb/users/recovery", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/recovery", func(w http.ResponseWriter, r *http.Request) {
 		api.RecoveryEmail(w, r, client, dbName, userCollection)
 	}).Methods("POST")
 
 	// Define route to send recovery email with a code to verify for backoffice
-	router.HandleFunc("/bo/users/recovery", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/recovery", func(w http.ResponseWriter, r *http.Request) {
 		api.RecoveryEmail(w, r, client, dbName, userCollection)
 	}).Methods("POST")
 
 	// Define route to confirm the code set by the user and define the new password for mobile
-	router.HandleFunc("/mb/users/recovery-confirmation", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/mb/users/recovery-confirmation", func(w http.ResponseWriter, r *http.Request) {
 		api.ConfirmAuthCode(client, dbName, userCollection, w, r)
 	}).Methods("POST")
 
 	// Define route to confirm the code set by the user and define the new password for backoffice
-	router.HandleFunc("/bo/users/recovery-confirmation", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("api/v1/bo/users/recovery-confirmation", func(w http.ResponseWriter, r *http.Request) {
 		api.ConfirmAuthCode(client, dbName, userCollection, w, r)
 	}).Methods("POST")
 }
