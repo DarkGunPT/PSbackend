7e1d9783e0f3a50974a2fba69bc70e82067787fe
Nuno Honório <nh@kamae.pt>
2024-12-08 16:36:24 +0000

    WHAT IS THIS GATEAWYDOIN

--- api/users.go
+++ api/users.go
@@ -607,6 +607,10 @@ func RegisterCompletion(client *mongo.Client, dbName, userCollection string, w h
 		WorkEnd      string               `json:"workEnd" bson:"workEnd"`
 	}
 
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("User updated successfully")
+	return
+
 	json.NewDecoder(r.Body).Decode(&requestBody)
 	if requestBody.Email == "" {
 		http.Error(w, "Email is required for update", http.StatusBadRequest)
